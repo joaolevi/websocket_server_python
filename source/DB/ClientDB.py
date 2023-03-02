a662a28dfcd96d912a87647fc7dbf039e4b479b0
joaolevi <joaolevi777@gmail.com>
2023-03-02 19:46:02 -0300

    Creating Some Data to DB

--- source/DB/ClientDB.py
+++ source/DB/ClientDB.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine('postgresql://joaolevi:1234@localhost:5432/postgres', echo=False)
+engine = create_engine('postgresql://postgres:1234@localhost:5432/postgres', echo=False)
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -16,4 +16,14 @@ class ClientDB(Base):
     age = Column(Integer)
     cpf = Column(String(50))
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
+
+client1 = ClientDB(name='John Rich', age=26, cpf='999.000.111-89')
+client2 = ClientDB(name='Mariana Raujo', age=28, cpf='222.030.113-52')
+client3 = ClientDB(name='Piter Parker', age=25, cpf='311.232.456-64')
+
+session.add(client1)
+
+session.add_all([client2, client3])
+
+session.commit()
